f407840307da0f86cb68e8df1404c7780ae3086d
Gbemileke Ayodele <onabajogbemileke37@gmail.com>
2024-07-31 14:42:18 -0700

    completed

--- _printf.c
+++ _printf.c
@@ -1,6 +1,41 @@
-#include <stdarg.h>
+#include <limits.h>
 #include <stdio.h>
-#include <unistd.h>
+#include "main.h"
+
+int _printf(const char *format, ...) {
+    va_list args;
+    int printed_chars = 0;
+    const char *traverse;
+
+    va_start(args, format);
+
+    for (traverse = format; *traverse != '\0'; traverse++) {
+        if (*traverse == '%') {
+            traverse++;
+            if (*traverse == 'c') {
+                char c = (char)va_arg(args, int);
+                write(1, &c, 1);
+                printed_chars++;
+            } else if (*traverse == 's') {
+                char *s = va_arg(args, char *);
+                while (*s) {
+                    write(1, s++, 1);
+                    printed_chars++;
+                }
+            } else if (*traverse == '%') {
+                char c = '%';
+                write(1, &c, 1);
+                printed_chars++;
+            }
+        } else {
+            write(1, traverse, 1);
+            printed_chars++;
+        }
+    }
+
+    va_end(args);
+    return printed_chars;
+}
 
 int _printf(const char *format, ...) {
     va_list args;
