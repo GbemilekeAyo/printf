226f28b41b430730ed24a103c8d410e1d4631306
Gbemileke Ayodele <onabajogbemileke37@gmail.com>
2024-07-31 15:09:20 -0700

    completed

--- _printf.c
+++ _printf.c
@@ -3,7 +3,15 @@
 #include <unistd.h>
 #include <limits.h>
 
-/* Function prototypes for helper functions */
+
+/**
+ * _printf - produces output according to a format
+ * @format: format string containing the characters and the specifiers
+ * Description: this function will call the get_print() function that will
+ * determine which printing function to call depending on the conversion
+ * specifiers contained into fmt
+ * Return: length of the formatted output string*/
+
 int print_char(char c);
 int print_string(char *s);
 int print_integer(int n);
